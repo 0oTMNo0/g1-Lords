c4e068649323176514224bba2c1604da3e148af5
taban mehr nabizadeh <taban.nbz@gmail.com>
2022-07-14 11:54:39 +0430

    0.5

--- front/front-lords/src/pages/Chat.jsx
+++ front/front-lords/src/pages/Chat.jsx
@@ -3,14 +3,38 @@ import React from 'react'
 function Chat() {
   return (
     <div className="flex flex-col">
-        <div className="overflow-y-scroll h-96">
+        <div className="overflow-y-scroll h-96 flex flex-col p-4 gap-3">
+
+            {/* admin-msg */}
+            <div className="flex flex-col w-2/3 shadow-lg rounded bg-slate-600 text-white text-xs">
+                <div className='border-b-2 items-start p-3'>
+                    <p>name</p>
+                    <p>date</p>
+                </div>
+                <div className='p-3'>
+                    <p>msg</p>
+                </div>
+            </div>
+
+
+
+            {/* user-msg */}
+            <div className="flex flex-col w-2/3 shadow-lg rounded bg-slate-400 text-white text-xs">
+                <div className='border-b-2 items-start p-3'>
+                    <p>name</p>
+                    <p>date</p>
+                </div>
+                <div className='p-3'>
+                    <p>msg</p>
+                </div>
+            </div>
 
         </div>
         <form className="p-3 border-t-2" onSubmit={(e)=>{
             e.preventDefault()
         }}>
             <textarea className="w-full bg-transparent" placeholder="enter message"></textarea>
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full">Send</button>
         </form>
       
     </div>
--- front/front-lords/src/pages/Home.jsx
+++ front/front-lords/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom'
 
 function Home() {
 
+    const [userName]
+
     const navigate = useNavigate()
 
     
@@ -10,7 +12,7 @@ function Home() {
   return (
     <>
         <div>
-            <form onSubmit={(e)=>{
+            <form className='p-4' onSubmit={(e)=>{
                 e.preventDefault()
                 navigate('./chat')
             }}>
@@ -20,9 +22,9 @@ function Home() {
                 </div>
                 <div class="mb-6">
                         <label for="rooms" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Rooms</label>
-                        <input type="text" id="rooms" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="E" required />
+                        <input type="text" id="rooms" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your room name" required />
                 </div>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Start Chat</button>
             </form>
         </div>
     </>
