c96a599f020b48abf5eb44e61ede3e66625fb4cd
taban mehr nabizadeh <taban.nbz@gmail.com>
2022-07-14 11:14:49 +0430

    0.2

--- front/front-Lords/src/App.js
+++ front/front-Lords/src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
 import './App.css';
+import {Routes, Route} from "react-router"
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
-    <div>
+    <div className="w-96 mx-auto bg-slate-600">
+      <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Chat />} />
+      </Routes>
     </div>
   );
 }
